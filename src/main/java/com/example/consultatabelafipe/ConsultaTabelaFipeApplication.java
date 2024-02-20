c9da04f5931756f561bb5091f902ef2db9ac13ea
f3l1pe-augusto <fa.ferreira@unesp.br>
2024-02-20 09:03:57 -0300

    First commit

---
+++ src/main/java/com/example/consultatabelafipe/ConsultaTabelaFipeApplication.java
@@ -0,0 +1,18 @@
+package com.example.consultatabelafipe;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaTabelaFipeApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaTabelaFipeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		System.out.println("Hello World!");
+	}
+}
