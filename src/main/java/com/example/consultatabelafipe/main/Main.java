1d316c60629f6497ba11e0729c8dbece1dd0b2a5
f3l1pe-augusto <fa.ferreira@unesp.br>
2024-03-04 08:48:44 -0300

    test

--- src/main/java/com/example/consultatabelafipe/main/Main.java
+++ src/main/java/com/example/consultatabelafipe/main/Main.java
@@ -1,15 +1,24 @@
 package com.example.consultatabelafipe.main;
 
+import com.example.consultatabelafipe.service.ApiUsage;
+
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
   private final Scanner scanner = new Scanner(System.in);
+  private final ApiUsage apiUsage = new ApiUsage();
 
   public void showMenu() {
-    System.out.println("\nDigite o tipo de veículo: 1 - Carro, 2 - Moto, 3 - Caminhão");
-    int vehicleType = scanner.nextInt();
 
-    System.out.println("Você escolheu: " + vehicleType);
+    System.out.print("\nDigite o tipo de veículo (carros, motos, caminhoes): ");
+    String vehicleType = scanner.nextLine().toLowerCase(Locale.ROOT);
+
+    String URL = "https://parallelum.com.br/fipe/api/v1/";
+    var json = apiUsage.getData(URL + vehicleType + "/marcas");
+
+    System.out.println(json);
+
   }
 }
